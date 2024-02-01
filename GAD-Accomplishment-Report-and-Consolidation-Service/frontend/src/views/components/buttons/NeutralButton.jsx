7af33fe98e4ac06ea9a66923e6cc208425cd8cc2
JanDwight <103998322+JanDwight@users.noreply.github.com>
2024-02-01 16:24:34 +0800

    Create Nutral Button Done

---
+++ GAD-Accomplishment-Report-and-Consolidation-Service/frontend/src/views/components/buttons/NeutralButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Submit({ label, onClick, /*disabled*/ }) {
+
+  return (
+    <div>
+      <button
+        type="submit"
+        className={`flex w-[100%] justify-center rounded-md bg-blue-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-400`}
+        //disabled={disabled}
+        onClick={onClick}
+      >
+        {label}
+      </button>
+    </div>
+  )
+}
