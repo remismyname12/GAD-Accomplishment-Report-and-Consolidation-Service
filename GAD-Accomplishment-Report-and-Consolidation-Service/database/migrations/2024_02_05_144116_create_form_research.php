bc8faf5dc8f3c8000005ca0cb96ac50e73e14997
remismyname12 <12ganase@gmail.com>
2024-02-06 16:38:10 +0800

    form_type_added

--- GAD-Accomplishment-Report-and-Consolidation-Service/database/migrations/2024_02_05_144116_create_form_research.php
+++ GAD-Accomplishment-Report-and-Consolidation-Service/database/migrations/2024_02_05_144116_create_form_research.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('form_research', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('form_type');
             $table->timestamps();
         });
     }
