591a7ad488177b8d27862b92e6b84cd466845552
JanDwight <103998322+JanDwight@users.noreply.github.com>
2024-02-08 13:04:05 +0800

    getAxios Now ready for get method migration

---
+++ GAD-Accomplishment-Report-and-Consolidation-Service/frontend/src/views/axios/axiosMethods/getAxios.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import axiosClient from "../axios";
+
+export default async function getAxios({ endPoint, data }) {
+    console.log('This is the data: ', data);
+    try {
+        const response = await axiosClient.post(endPoint, data);
+        return { response };
+    } catch (error) {
+        if (error.response) {
+            const { message, errors } = error.response.data;
+            const finalErrors = [];
+            // Push each error message to finalErrors array
+            for (const key in errors) {
+                if (errors.hasOwnProperty(key)) {
+                    finalErrors.push(...errors[key]);
+                }
+            }
+            // Construct the error object with HTML format
+            return { error: { __html: `${message} (${finalErrors.length} errors)<br>${finalErrors.join("<br>")}` } };
+        } else {
+            return { error: { __html: "An unexpected error occurred." } };
+        }
+    }
+}
