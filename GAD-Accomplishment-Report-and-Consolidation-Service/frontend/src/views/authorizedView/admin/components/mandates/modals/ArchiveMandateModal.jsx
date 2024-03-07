7bda456d29853f21b5a8d9900de31593a6d34691
JanDwight <103998322+JanDwight@users.noreply.github.com>
2024-03-07 10:50:25 +0800

    Update and Archive done

--- GAD-Accomplishment-Report-and-Consolidation-Service/frontend/src/views/authorizedView/admin/components/mandates/modals/ArchiveMandateModal.jsx
+++ GAD-Accomplishment-Report-and-Consolidation-Service/frontend/src/views/authorizedView/admin/components/mandates/modals/ArchiveMandateModal.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import { React, useState} from 'react'
+import Submit from '../../../../../components/buttons/Submit';
+import axiosClient from '../../../../../axios/axios';
 
-export default function ArchiveMandateModal() {
+//---------For Feedback
+import Feedback from '../../../../../components/feedbacks/Feedback';
+
+export default function ArchiveMandateModal({ mandateSelected }) {
+      // For feedback
+  const [error, setError] = useState('');
+  const [message, setAxiosMessage] = useState('');
+  const [status, setAxiosStatus] = useState('');
+
+  const onSubmit = async (ev) => {
+    ev.preventDefault();
+    setError({ __html: "" });
+
+    try {
+      const response = await axiosClient.put(`/archivemandate/${mandateSelected[0].id}`, {});
+      setAxiosMessage(response.data.message); // Set success message
+      setAxiosStatus(response.data.Success);
+      setTimeout(() => {
+        setAxiosMessage(''); // Clear success message
+        setAxiosStatus('');
+      }, 3000); // Timeout after 3 seconds
+    } catch (error) {
+      setAxiosMessage(error.response.data.message); // Set success message
+    }
+  };
   return (
-    <div>ArchiveMandateModal</div>
+    <div>
+      {/** For Feedback */}
+      <Feedback isOpen={message !== ''} onClose={() => setAxiosMessage('')} successMessage={message} status={status} />
+
+      <h1>
+        Are you sure you want to delete <b>{mandateSelected[0].gender_issue}</b>
+      </h1>
+      {/** BUTTONS */}
+      <div className='mt-5'>
+        <Submit label="Archive Activity Design" onClick={onSubmit} /* disabled={your condition} */ />
+      </div>
+    </div>
   )
 }
