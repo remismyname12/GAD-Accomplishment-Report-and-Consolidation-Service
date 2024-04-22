7fbcb665fec9eb22a5e898460877ec7aee9a6e5b
JanDwight <103998322+JanDwight@users.noreply.github.com>
2024-04-22 10:37:25 +0800

    Updated the position of the loading

--- GAD-Accomplishment-Report-and-Consolidation-Service/frontend/src/views/components/feedbacks/LoadingHorizontalLine.jsx
+++ GAD-Accomplishment-Report-and-Consolidation-Service/frontend/src/views/components/feedbacks/LoadingHorizontalLine.jsx
@@ -6,7 +6,7 @@ const LoadingHorizontalLine = ({ isLoading }) => {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center pt-5 h-screen">
       <div className="mb-4">Loading...</div>
       <div className="relative w-full h-2 rounded-full overflow-hidden bg-gray-300">
         <div className="absolute top-0 left-0 w-full h-2 bg-blue-500 rounded-full animate-pulse"></div>
